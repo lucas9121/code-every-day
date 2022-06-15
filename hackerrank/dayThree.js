e30e7fb7111090579a8860e3cf319941b010730b
Lucas <lucas2carlos@gmail.com>
2022-06-15 19:16:00 -0400

    solved birthday Cake Candles challenge

--- hackerrank/dayThree.js
+++ hackerrank/dayThree.js
@@ -29,3 +29,13 @@ function timeConversion(s) {
 }
 
 timeConversion('07:05:45PM')
+
+
+//You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
+
+function birthdayCakeCandles(candles) {
+    const maxHeight = Math.max(...candles)
+    return (candles.filter((candle) => candle === maxHeight)).length
+}
+
+birthdayCakeCandles([3, 2, 1, 3])
