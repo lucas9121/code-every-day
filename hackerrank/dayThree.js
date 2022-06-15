9b5f097395f74466d2d8efed4f21d68c7f097ef8
Lucas <lucas2carlos@gmail.com>
2022-06-15 19:16:28 -0400

    solved grading students challenge

--- hackerrank/dayThree.js
+++ hackerrank/dayThree.js
@@ -39,3 +39,24 @@ function birthdayCakeCandles(candles) {
 }
 
 birthdayCakeCandles([3, 2, 1, 3])
+
+// HackerLand University has the following grading policy:
+// Every student receives a grade in the inclusive range from  to .
+// Any grade less than 40 is a failing grade.
+// Sam is a professor at the university and likes to round each student's grade according to these rules:
+// If the difference between the grade and the next multiple of 5 is less than 3, round grade up to the next multiple of 5.
+// If the value of grade is less than 38, no rounding occurs as the result will still be a failing grade.
+
+
+function gradingStudents(grades) {
+    let finalGrades = grades
+    finalGrades.forEach((grade, i) => {
+        if(grade > 37 && grade %5 > 2){
+            let newGrade = (Math.round(grade / 5)) * 5
+            finalGrades.splice(i, 1, newGrade)
+        }
+    })
+    return finalGrades
+}
+
+gradingStudents([73, 67, 40, 33])
