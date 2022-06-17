279ad49e72e49653b34f3c3812c82d5f6171ace5
Lucas <lucas2carlos@gmail.com>
2022-06-16 23:31:06 -0400

    solved convert string to camel case  challenge

--- code-wars/dayFour.js
+++ code-wars/dayFour.js
@@ -11,4 +11,19 @@ function alternate(n, firstValue, secondValue){
 
 alternate(10, "blue", "red")
 
+// Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
+
+function toCamelCase(str){
+    const arr = str.split('')
+    for (let i = 0; i < arr.length; i++){
+      if(arr[i] === "-" || arr[i] === "_"){
+        arr[i + 1] = arr[i + 1].toString().toUpperCase()
+        arr.splice(i, 1)
+      }
+    }
+    return arr.join('')
+}
+
+toCamelCase("the_stealth_warrior")
+
 
