b7a75506c2d6936a3c5f51ab1b8bceb767e98c1a
Lucas <lucas2carlos@gmail.com>
2022-06-17 18:22:11 -0400

    solved number line jumps

--- hackerrank/dayFour.js
+++ hackerrank/dayFour.js
@@ -20,3 +20,23 @@ function countApplesAndOranges(s, t, a, b, apples, oranges) {
     })
     console.log(myApples.length + '\n' + myOranges.length)
 }
+
+//You are choreographing a circus show with various animals. For one act, you are given two kangaroos on a number line ready to jump in the positive direction (i.e, toward positive infinity).
+// The first kangaroo starts at location  and moves at a rate of  meters per jump.
+// The second kangaroo starts at location  and moves at a rate of  meters per jump.
+// You have to figure out a way to get both kangaroos at the same location at the same time as part of the show. If it is possible, return YES, otherwise return NO.
+
+function kangaroo(x1, v1, x2, v2) {
+    // if(x1 > x2 && v1 < v2 || x1 < x2 && v1 > v2) return "YES"
+    // if(x1 === x2 && v1 === v2) return "YES"
+    for (let i = 0; i < 10000; i++){
+        x1 += v1
+        console.log(`x1 is ${x1}`)
+        x2 += v2
+        console.log(`x2 is ${x2}`)
+        if(x1 === x2) return "YES"
+    }
+    return "NO"
+}
+
+kangaroo(0, 3, 4, 2)
