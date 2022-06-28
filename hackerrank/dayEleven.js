c404a764ea838b6ec4fe051327b68b1512de668d
Lucas <lucas2carlos@gmail.com>
2022-06-27 22:33:53 -0400

    adding description

--- hackerrank/dayEleven.js
+++ hackerrank/dayEleven.js
@@ -1,4 +1,6 @@
-//Dictionaries and Maps
+// Dictionaries and Maps
+// Given n names and phone numbers, assemble a phone book that maps friends' names to their respective phone numbers. You will then be given an unknown number of names to query your phone book for. For each name queried, print the associated entry from your phone book on a new line in the form name=phoneNumber; if an entry for name is not found, print Not found instead.
+// Note: Your phone book should be a Dictionary/Map/HashMap data structure.
 
 function processData(input) {
     const inputArr = input.split('\n')
@@ -16,3 +18,5 @@ function processData(input) {
         }
     }
 } 
+
+processData("3\nsam 99912222\ntom 11122222\nharry 12299933\nsam\nedward\nharry")
